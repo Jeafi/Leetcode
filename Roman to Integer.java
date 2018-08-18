86a2dc737b71d7689f2d96ef2931b55e2e6d8282
Jeafi <651699394@qq.com>
2018-08-18 19:19:31 +0800

    Roman to Integer

---
+++ Roman to Integer.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int romanToInt(String s) {
+        Map<Character,Integer> roams = new HashMap<>();
+        roams.put('I',1);
+        roams.put('V',5);
+        roams.put('X',10);
+        roams.put('L',50);
+        roams.put('C',100);
+        roams.put('D',500);
+        roams.put('M',1000);
+        int sum =0;
+        int i=0;
+        while (i<s.length()-1){
+            int j = i+1;
+            char char1 = s.charAt(i);
+            char char2 = s.charAt(j);
+            int num1 = roams.get(char1);
+            int num2 = roams.get(char2);
+            if(num2>num1){
+                sum += (num2 -num1);
+                i+=2;
+            }else{
+                sum+=num1;
+                i++;
+            }
+        }
+        if(i<s.length()){
+            sum += roams.get(s.charAt(s.length()-1));
+        }
+        return sum;
+    }
+}
